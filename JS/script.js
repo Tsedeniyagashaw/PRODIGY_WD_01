ae67717bbe7f1ed879997ad8728df06c41506826
Tsedeniyagashaw <tsedeniyagashaw672@gmail.com>
2025-03-11 06:10:59 +0300

    NavBar

--- JS/script.js
+++ JS/script.js
@@ -1,19 +1,24 @@
 const navBar = document.querySelector('.nav-bar-container');
 const navItems = document.querySelectorAll('.nav-bar li a');
-
+const sections = document.querySelectorAll('.page');
 
 function scrollEffect() {
-    if (window.scrollY > 100) {
-        navBar.style.backgroundColor = 'blue';
+    let scrollPosition = window.scrollY;
+
+ if (scrollPosition > 1200) {
+        navBar.style.background = "linear-gradient(to right,rgb(7, 45, 83),rgb(1, 75, 124))"; 
+    } else if (scrollPosition > 600) {
+        navBar.style.background = "linear-gradient(to right,rgb(7, 45, 83),rgb(26, 2, 63))"; 
     } else {
-        navBar.style.backgroundColor = 'darkblue';
+        navBar.style.background = "linear-gradient(to right,rgb(7, 45, 83),rgb(5, 43, 30))"; 
     }
 }
 
 function hoverEffect(event) {
     event.target.style.textDecoration = 'underline';
-    event.target.style.color = 'lightblue';
+    event.target.style.color = 'yellow';
 }
+
 
 function unhoverEffect(event) {
     event.target.style.textDecoration = 'none';
@@ -21,10 +26,17 @@ function unhoverEffect(event) {
 }
 
 
-window.addEventListener('scroll', scrollEffect);
 
 
-navItems.forEach(link => {
-    link.addEventListener('mouseenter', hoverEffect);  
-    link.addEventListener('mouseout', unhoverEffect); 
+
+
+window.addEventListener("scroll", () => {
+    scrollEffect();
+    revealSections();
 });
+navItems.forEach(link => {
+    link.addEventListener("mouseenter", hoverEffect);
+    link.addEventListener("mouseleave", unhoverEffect);
+});
+
+
